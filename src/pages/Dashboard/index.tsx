2b9542c241792f6c4eab2c0ab932fd70595afeee
andregraciano16 <andregraciano16@gmail.com>
2020-07-25 16:22:07 -0300

    estilizando dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,55 @@
 import React from 'react';
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { FiChevronRight } from 'react-icons/fi'
 const Dashboard: React.FC = () => {
-    return <Title>Dashboard</Title>
-}
+    return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer"/>
+      <Title>Dashboard</Title>
+      <Form>
+          <input placeholder="Digite o nome do repositório aqui" />
+          <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+            <img
+                src="https://miro.medium.com/max/383/1*co_1qORNdM0PI1nvCp7Iig.png"
+                alt="Andre Graciano" />
+            <div>
+                <strong>rocketseat/uniform</strong>
+                <p>Repositorio do git e muito legal</p>
+            </div>
+            <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+            <img
+                src="https://miro.medium.com/max/383/1*co_1qORNdM0PI1nvCp7Iig.png"
+                alt="Andre Graciano" />
+            <div>
+                <strong>rocketseat/uniform</strong>
+                <p>Repositorio do git e muito legal</p>
+            </div>
+            <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+            <img
+                src="https://miro.medium.com/max/383/1*co_1qORNdM0PI1nvCp7Iig.png"
+                alt="Andre Graciano" />
+            <div>
+                <strong>rocketseat/uniform</strong>
+                <p>Repositorio do git e muito legal</p>
+            </div>
+            <FiChevronRight size={20} />
+        </a>
+
+      </Repositories>
+
+    </>
+    );
+};
 
 export default Dashboard;
