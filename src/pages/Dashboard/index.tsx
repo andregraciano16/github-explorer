270a82f347718a737df13e0c721612867668f427
andregraciano16 <andregraciano16@gmail.com>
2020-07-25 17:33:56 -0300

    conectando com a API do GitHub

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,51 +1,64 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { Title, Form, Repositories } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
 import logoImg from '../../assets/logo.svg';
-import { FiChevronRight } from 'react-icons/fi'
+import Repository from '../Repository';
+
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    }
+
+}
+
 const Dashboard: React.FC = () => {
+
+    const [ newRepo, setNewRepo ] = useState('');
+    const [ repositories, setRepositories ] = useState<Repository[]>([]);
+
+    async function handleAddRepository (event: FormEvent<HTMLFormElement>): Promise<void> {
+        event.preventDefault();
+
+        const response = await api.get<Repository>(`repos/${newRepo}`);
+
+        const repository = response.data;
+
+        setRepositories([ ...repositories, repository]);
+
+        setNewRepo('');
+    }
+
     return (
     <>
       <img src={logoImg} alt="GitHub Explorer"/>
       <Title>Dashboard</Title>
-      <Form>
-          <input placeholder="Digite o nome do repositório aqui" />
+      <Form onSubmit={handleAddRepository}>
+          <input
+            value={newRepo}
+            onChange={(e) => setNewRepo(e.target.value)}
+            placeholder="Digite o nome do repositório aqui" />
           <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-            <img
-                src="https://miro.medium.com/max/383/1*co_1qORNdM0PI1nvCp7Iig.png"
-                alt="Andre Graciano" />
-            <div>
-                <strong>rocketseat/uniform</strong>
-                <p>Repositorio do git e muito legal</p>
-            </div>
-            <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-            <img
-                src="https://miro.medium.com/max/383/1*co_1qORNdM0PI1nvCp7Iig.png"
-                alt="Andre Graciano" />
-            <div>
-                <strong>rocketseat/uniform</strong>
-                <p>Repositorio do git e muito legal</p>
-            </div>
-            <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-            <img
-                src="https://miro.medium.com/max/383/1*co_1qORNdM0PI1nvCp7Iig.png"
-                alt="Andre Graciano" />
-            <div>
-                <strong>rocketseat/uniform</strong>
-                <p>Repositorio do git e muito legal</p>
-            </div>
-            <FiChevronRight size={20} />
-        </a>
-
+        { repositories.map(repository => (
+            <a key= {repository.full_name} href="teste">
+                <img
+                    src={repository.owner.avatar_url}
+                    alt= {repository.owner.login} />
+                <div>
+                    <strong>{repository.full_name}</strong>
+                    <p>{repository.description}</p>
+                </div>
+                <FiChevronRight size={20} />
+            </a>
+            ))
+        }
       </Repositories>
 
     </>
