a29461ea5fb0ac61298b3c27d7bacefe7cc7d188
andregraciano16 <andregraciano16@gmail.com>
2020-07-25 23:18:23 -0300

    salvando repositorios no storage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { Title, Form, Repositories, Error } from './styles';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
@@ -18,22 +18,35 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
 
-    const [ newRepo, setNewRepo ] = useState('');
-    const [ inputError, setInputError ] = useState('');
-    const [ repositories, setRepositories ] = useState<Repository[]>([]);
+    const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storageRepoitories = localStorage.getItem('@GithubExplorer:repositories');
+        if (storageRepoitories) {
+            return JSON.parse(storageRepoitories);
+        }
+        return [];
+    });
 
-    async function handleAddRepository (event: FormEvent<HTMLFormElement>): Promise<void> {
+    useEffect(() => {
+        localStorage.setItem(
+            '@GithubExplorer:repositories',
+            JSON.stringify(repositories),
+        );
+    }, [repositories]);
+
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault();
 
-        if(!newRepo) {
+        if (!newRepo) {
             setInputError('Digite o autor/nome do repositório');
-            return ;
+            return;
         }
 
         try {
             const response = await api.get<Repository>(`repos/${newRepo}`);
             const repository = response.data;
-            setRepositories([ ...repositories, repository]);
+            setRepositories([...repositories, repository]);
             setNewRepo('');
             setInputError('');
         } catch (err) {
@@ -42,36 +55,36 @@ const Dashboard: React.FC = () => {
     }
 
     return (
-    <>
-      <img src={logoImg} alt="GitHub Explorer"/>
-      <Title>Dashboard</Title>
-      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
-          <input
-            value={newRepo}
-            onChange={(e) => setNewRepo(e.target.value)}
-            placeholder="Digite o nome do repositório aqui" />
-          <button type="submit">Pesquisar</button>
-      </Form>
+        <>
+            <img src={logoImg} alt="GitHub Explorer" />
+            <Title>Dashboard</Title>
+            <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+                <input
+                    value={newRepo}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome do repositório aqui" />
+                <button type="submit">Pesquisar</button>
+            </Form>
 
-      {inputError && <Error>{inputError}</Error>}
+            {inputError && <Error>{inputError}</Error>}
 
-      <Repositories>
-        { repositories.map(repository => (
-            <a key= {repository.full_name} href="teste">
-                <img
-                    src={repository.owner.avatar_url}
-                    alt= {repository.owner.login} />
-                <div>
-                    <strong>{repository.full_name}</strong>
-                    <p>{repository.description}</p>
-                </div>
-                <FiChevronRight size={20} />
-            </a>
-            ))
-        }
-      </Repositories>
+            <Repositories>
+                {repositories.map(repository => (
+                    <a key={repository.full_name} href="teste">
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login} />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))
+                }
+            </Repositories>
 
-    </>
+        </>
     );
 };
 
