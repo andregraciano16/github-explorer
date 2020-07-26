c82141066a37fe23b2059b508f794d9be788815c
andregraciano16 <andregraciano16@gmail.com>
2020-07-26 00:27:21 -0300

    estilizando tela de detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,13 +1,58 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
-
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
 interface RepositoryParams {
     repository: string;
 }
 
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
-    return <h1>Repository:{params.repository}</h1>;
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                Voltar
+                </Link>
+            </Header>
+            <RepositoryInfo>
+                <header>
+                    <img src="https://miro.medium.com/max/383/1*co_1qORNdM0PI1nvCp7Iig.png" alt="rocketseat" />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>descrição do repositorio</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1800</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to="asdfasd" >
+                    <div>
+                        <strong>fgsdfgsdfgsdf</strong>
+                        <p>sdfgsfdgsfgd</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    );
 }
 
 export default Repository;
